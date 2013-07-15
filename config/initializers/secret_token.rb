7a1ec521bfcdf0ce73dc6886419057631d6edc8c
Chris Smith <quartzmo@gmail.com>
2013-07-15 16:01:28 -0600

    rails new heart-rate-visualization-demo --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeartRateVisualizationDemo::Application.config.secret_key_base = '8c0423f329998f9c82a9bc8d5cbd719a1a4012474e84b5ecbb6298087ca0349ce186cc74ec16d26d50fcd68285e49acee3bd44aaa42305adf6c9e9e29d219ea8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeartRateVisualizationDemo::Application.config.session_store :cookie_store, key: '_heart-rate-visualization-demo_session'
