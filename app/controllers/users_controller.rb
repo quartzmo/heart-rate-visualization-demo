174d113262efb04f77981c91be2dc33e97afa273
Chris Smith <quartzmo@gmail.com>
2013-07-15 19:41:43 -0600

    Added static bootstrap from gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).limit(10)
+    @users = User.order(:id).paginate(:page => params[:page])
   end
 
   def show
